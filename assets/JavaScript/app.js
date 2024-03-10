de5adcb399121b10d33317d8c8bb0435835c0cf8
Elvin <elvinsrkrov@gmail.com>
2024-03-10 16:49:10 +0400

    page navigation has implemented

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -5,6 +5,8 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const navLinks = document.querySelector(".nav__links");
+const allNavLink = document.querySelectorAll(".nav__link");
 const nav = document.querySelector(".nav");
 const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
@@ -19,9 +21,17 @@ const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
+const pageNavigation = function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+};
 btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+navLinks.addEventListener("click", pageNavigation);
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
